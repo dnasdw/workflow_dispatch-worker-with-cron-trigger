d0082c5950da3d2030c513ee4318781ef7cb8aa8
Sun Daowen <sundaowen@gmail.com>
2022-01-05 17:12:05 +0800

    实现触发功能

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+// references:
+//   github actions:
+//     https://docs.github.com/actions/learn-github-actions/events-that-trigger-workflows#workflow_dispatch
+//     https://docs.github.com/rest/reference/actions#create-a-workflow-dispatch-event
+
+// https://docs.github.com/rest/overview/resources-in-the-rest-api
+const GITHUB_API_URL = "https://api.github.com";
+
+addEventListener('scheduled', event => {
+  event.waitUntil(triggerEvent(event.scheduledTime))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
-async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+
+async function triggerEvent(scheduledTime) {
+  await fetch(`${GITHUB_API_URL}/repos/${GITHUB_OWNER}/${GITHUB_REPO}/actions/workflows/${GITHUB_WORKFLOW_FILE_NAME}/dispatches`, {
+    method: "POST",
+    headers: {
+      "Authorization": `token ${GITHUB_TOKEN}`,
+      "Accept": "application/vnd.github.v3+json",
+      "Content-Type": "application/json",
+      "User-Agent": "Cloudflare-Workers"
+    },
+    body: JSON.stringify({
+      "ref": `${GITHUB_BRANCH}`,
+      "inputs": {
+        "trigger": "continue"
+      }
+    })
+  });
 }
